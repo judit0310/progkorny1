4c2da2b53d928bdb6b0d8bee83df43982779e3aa
judit0310 <jucika9403>
2019-10-03 13:12:24 +0200

    Playing with array, list, set, file and jackson-databind

--- src/test/java/DiakTest.java
+++ src/test/java/DiakTest.java
@@ -1,8 +1,11 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -59,5 +62,101 @@ public class DiakTest {
 
         }
     }
+
+    @Test
+    public void tombTest(){
+        String[] tomb= new String[5];
+        for(int i=0;i<tomb.length;i++){
+            tomb[i]=String.valueOf(i);
+        }
+        System.out.println(tomb);
+        for(int i =0; i<tomb.length;i++){
+            System.out.println(tomb[i]);
+        }
+
+    }
+
+    @Test
+    public void listTest(){
+        List<String> lista;
+        lista=new ArrayList<>();
+        for(int i=0; i < 10; i++){
+            lista.add(i, i+". cica");
+        }
+        for(int i=lista.size()-1; i >= 0; i--){
+            System.out.println(lista.get(i));
+        }
+        System.out.println(lista.contains("3. cica"));
+        System.out.println(lista.indexOf("6. cica"));
+
+        for(String s: lista){
+            System.out.println(s);
+        }
+        List ujlista =lista.subList(2, 4);
+        System.out.println(ujlista);
+
+        lista.set(8, "KACSA");
+        System.out.println(lista);
+    }
+
+    @Test
+    public void halmazTest(){
+        Set<String> halmaz = new HashSet<>(10);
+        halmaz.add("1");
+        halmaz.add("KISCICA");
+        for (int i =0; i< 10;i++){
+            halmaz.add("i. 123");
+            halmaz.add("i. 123");
+        }
+        System.out.println(halmaz);
+
+    }
+
+    @Test
+    public void jsonTest(){
+        File file;
+        file=new File(".idea");
+        if(file.exists()){
+            System.out.println("CICA");
+        }
+        else{
+            try {
+                System.out.println(file.mkdir());
+                file.createNewFile();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(file.isDirectory());
+        List<File> files =Arrays.asList(file.listFiles());
+        for(File f: files){
+            System.out.println(f.getName());
+        }
+        System.out.println(file.listFiles());
+        System.out.println(file.getAbsoluteFile());
+        System.out.println(file.getName());
+
+        File kiiro = new File("kiscica.json");
+        if(!kiiro.exists()){
+            try {
+                kiiro.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        Collection<Diak> diakok = new ArrayList<>();
+        Diak bela = new Diak();
+        bela.setNev("KISS BELA");
+        diakok.add(bela);
+        try {
+            mapper.writeValue(kiiro, diakok);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
